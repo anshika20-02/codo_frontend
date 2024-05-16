9f816f314fde2282c4d7d5f1906fcda36bb06164
anshika20-02 <bishtrenu465@gmail.com>
2024-05-16 14:32:48 +0530

    CodoFrontend

---
+++ frontend/src/Components/HomepageScreen/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+  return (
+    <> 
+        <div class="wave-container wave">
+            <p>© {year}, Developed & Designed by Team CodoSpace</p>
+        </div>
+    </>
+  )
+}
+
+export default Footer
